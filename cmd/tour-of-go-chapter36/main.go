bfea6490ba78835784e02875fa1376bfd345ed24
seongjumoon <sj19990@naver.com>
2019-02-25 23:57:59 +0900

    new chapter of map

---
+++ cmd/tour-of-go-chapter36/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 다음과 같이 함수내의 맵 타입을 정의하여 사용할 수 있다.
+
+func main() {
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+}
